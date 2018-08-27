79089dd29ace13ca4424febd45fe1deaac974a04
Daniel Aguilera <twinber@hotmail.com>
2018-08-27 14:27:33 +0200

    inicio con readme y scripts de travis

---
+++ README.md
@@ -0,0 +1,14 @@
+# Plugins Flutter
+
+[![Build Status](https://travis-ci.org/flutter-es/ejemplos.svg?branch=master)](https://travis-ci.org/flutter-es/ejemplos)
+
+Una colección de plugins creados y/o mantendidos por FlutterES
+
+## Indice
+
+TODO
+
+## ¿Interesado en contribuir?
+
+TODO
+
---
+++ travis_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Normalize Branch variable
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+# Analyze plugins
+
+for D in *; do
+
+  if [ -d "${D}" ]; then
+    cd ${D}
+    echo "Run flutter packages get on ${D}"
+    ../flutter/bin/flutter packages get 
+		
+		echo "Run flutter analyze on ${D}"
+		../flutter/bin/flutter analyze 
+    cd .. 
+
+  fi
+done
+
+
+# pass test
+# declare -a  PROJECT_NAMES=(
+# 	# "jsonexample" \
+# 	# "shrine" \
+# )
+
+# for PROJECT_NAME in "${PROJECT_NAMES[@]}"
+# do
+# 	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
+# 	pushd "${PROJECT_NAME}"
+# 	../flutter/bin/flutter test
+# 	popd
+# done
+
+# echo "-- Success --"
